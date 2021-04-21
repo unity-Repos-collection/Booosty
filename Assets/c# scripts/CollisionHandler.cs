af2a24104baafbbe20e21502fb8b0dfd09d3f42d
Luca Farci <lfarci09@gmail.com>
2021-04-21 21:57:25 +0100

    New level and iteration over  levelArray

--- Assets/c# scripts/CollisionHandler.cs
+++ Assets/c# scripts/CollisionHandler.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) 
+    
+    
+    
+    void OnCollisionEnter(Collision other) 
     {
+    
+    
     switch (other.gameObject.tag)
     {
         case "Friendly":
@@ -11,15 +17,34 @@ public class CollisionHandler : MonoBehaviour
             break;
         
         case "Finish":
-            Debug.Log("Hit Finish");
+            //Debug.Log("Hit Finish");
+            NextLevel();
             break;
-        
         default:
-            Debug.Log("Explode You Cunt");
+            //Debug.Log("Explode You Cunt");
+            Invoke("ReloadLevel", 1f);
             break;
+    }
+    }
+    void NextLevel()
+    {   
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int NextSceneIndex = CurrentSceneIndex + 1;
+        if (NextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            NextSceneIndex = 0;    
+        } 
+        SceneManager.LoadScene(NextSceneIndex);
+    
+
+    }
+    void ReloadLevel()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex);
     }
 
-        
-    }
+
+
 
 }
--- Assets/c# scripts/Movement.cs
+++ Assets/c# scripts/Movement.cs
@@ -44,11 +44,6 @@ public class Movement : MonoBehaviour
 
     }
     
-    
-    
-    
-    
-    
     void ProcessRotation()
     {   
         //rotate
